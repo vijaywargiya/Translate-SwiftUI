862ea2c3c9b644d6f4944c514cbbfa19219c23a9
Shivam Vijaywargiya <shivamvijaywargiya@gmail.com>
2020-07-18 11:52:55 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Shivam Vijaywargiya on 18/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/translateApp.swift
@@ -0,0 +1,17 @@
+//
+//  translateApp.swift
+//  Shared
+//
+//  Created by Shivam Vijaywargiya on 18/07/20.
+//
+
+import SwiftUI
+
+@main
+struct translateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
